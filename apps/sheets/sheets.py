f89881d3a80e6c1d366d2bc85b800bab379fa2c4
mirrorkeydev <mirrorkeydev@gmail.com>
2024-07-08 22:44:07 -0700

    Make it even better woah

--- apps/sheets/sheets.py
+++ apps/sheets/sheets.py
@@ -1,4 +1,5 @@
-from talon import Module
+from talon import Module, clip, actions
+import re
 
 from .sheet_captures import Cell, CellRange
 
@@ -13,10 +14,50 @@ mod.apps.google_sheets = r"""
 app.name: Sheets
 """
 
+def get_current_location():
+    actions.edit.copy()
+    actions.sleep("100ms")
+    location = clip.text()
+    match = re.match(r"([A-Za-z]+)(\d+)", location)
+    old_column = match.group(1)
+    old_row = match.group(2)
+    return [old_column, old_row]
+
 @mod.action_class
 class Actions:
+    def select_row(row: int):
+        """Maintaining the current column position, select the given row."""
+        actions.user.chrome_mod("j")
+        old_column, old_row = get_current_location()
+        actions.insert(f"{old_column}{row}")
+        actions.key("enter")
+
+    def select_column(col: str):
+        """Maintaining the current row position, select the given column."""
+        actions.user.chrome_mod("j")
+        old_column, old_row = get_current_location()
+        actions.key("backspace")
+        print(old_column, old_row)
+        actions.insert(f"{col}{old_row}")
+        actions.key("enter")
+
     def select_cell(cell: Cell):
         """Selects the cell, without placing the cursor inside."""
+        actions.user.chrome_mod("j")
+        actions.key("backspace")
+        actions.sleep("50ms")
+        actions.user.insert_formatted(cell.column, "ALL_CAPS")
+        actions.insert(cell.row)
+        actions.key("enter")
 
     def select_cell_range(cell_range: CellRange):
         """Selects the cell range, without placing the cursor inside."""
+        actions.user.chrome_mod("j")
+        actions.key("backspace")
+        actions.sleep("50ms")
+        actions.user.insert_formatted(cell_range.start.column, "ALL_CAPS")
+        actions.insert(cell_range.start.row)
+        actions.insert(":")
+        actions.user.insert_formatted(cell_range.end.column, "ALL_CAPS")
+        actions.insert(cell_range.end.row)
+        actions.key("enter")
--- apps/sheets/sheets.talon
+++ apps/sheets/sheets.talon
@@ -2,11 +2,16 @@ app: google_sheets
 -
 tag(): browser # to enable rango in Sheet's PWA
 
-# Ex: `take air two` selects A2
-take <user.cell>: user.select_cell(cell)
+# Navigation
 
-# Ex: `take air two past bat 4` selects A2:B4
-take <user.cell_range>: user.select_cell_range(cell_range)
+row <user.number_string>: user.select_row(number_string)
+col <user.letters>: user.select_column(letters)
+# Ex: `call air two` selects A2
+cell <user.cell>: user.select_cell(cell)
+# Ex: `cell air two past bat fourteen` selects A2:B14
+cell <user.cell_range>: user.select_cell_range(cell_range)
+
+# Misc
 
 please <user.prose>:
     key(alt-/)
